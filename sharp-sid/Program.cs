db8b05c90d46e98e46dceaaabd4af797bb4dcfc0
maikroservice <maikroservice@WORKSTATION01>
2024-02-16 05:37:40 -0800

    Add project files.

---
+++ sharp-sid/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace sharp_sid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string unknownSID = args[0];
+            string objSID = "";
+
+            try
+            {
+                // create a user object and parse the unknown SID
+                objSID = new SecurityIdentifier(unknownSID).Translate(typeof(NTAccount)).ToString();
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+            }
+
+            // write username to stdout
+            Console.WriteLine(objSID);
+        }
+    }
+}
